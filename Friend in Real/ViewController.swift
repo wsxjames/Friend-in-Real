dadff1865dcf63b7861d71070f8af348f365ae67
wsxjames <wsxjames@163.com>
2020-03-14 22:13:03 -0400

    Initial Commit

---
+++ Friend in Real/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Friend in Real
+//
+//  Created by 吴世欣 on 3/14/20.
+//  Copyright © 2020 James Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
